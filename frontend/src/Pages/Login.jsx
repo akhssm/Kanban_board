679dcbf9a68312fdaedd345568e546f191038407
Akshay <mandaakshay20@gmail.com>
2025-01-02 15:29:15 +0530

    Made some changes.

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div>
+     <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>;
 };
 
 export default Login;
