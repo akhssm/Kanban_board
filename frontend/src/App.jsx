c5f1c8ba12beeb18198382c3b86401650138eddc
Akshay <mandaakshay20@gmail.com>
2025-01-29 15:37:33 +0530

    Created navbar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Trash from "./pages/Trash";
 import TaskDetails from "./pages/TaskDetails";
 import { Toaster } from "sonner";
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 function Layout() {
   const { user } = useSelector((state) => state.auth);
@@ -23,7 +24,7 @@ function Layout() {
       {/* <MobileSidebar/> */}
 
       <div className='flex-1 overflow-y-auto'>
-        {/*  <Navbar/> */}
+         <Navbar/>
 
         <div className='p-4 2x1:px-10'>
           <Outlet />
